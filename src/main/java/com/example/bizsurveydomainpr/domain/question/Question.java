cb5127be754e0882b18b856055bfc1e465f0fd0c
tud0 <psy563012@gmail.com>
2023-11-02 16:13:06 +0900

    question entity 추가, survey entity 수정

---
+++ src/main/java/com/example/bizsurveydomainpr/domain/question/Question.java
@@ -0,0 +1,36 @@
+package com.example.bizsurveydomainpr.domain.question;
+
+
+import com.example.bizsurveydomainpr.domain.survey.Survey;
+import com.example.bizsurveydomainpr.domain.user.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "question")
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String surveyQuestion;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private String answerType;
+
+    private int score;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "survey_id")
+    private Survey survey;
+
+}
