bfc0fdc52c2ba68fe8960cd476fef329ee385042
saul <dlathf3210@naver.com>
2023-11-02 15:56:49 +0900

    [FEAT] : add ChildCommentEntity

---
+++ src/main/java/com/example/bizsurveydomainpr/domain/childComment/ChildComment.java
@@ -0,0 +1,31 @@
+package com.example.bizsurveydomainpr.domain.childComment;
+
+import com.example.bizsurveydomainpr.domain.comment.Comment;
+import com.example.bizsurveydomainpr.domain.user.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "child_comment")
+public class ChildComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "child_comment_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    Comment comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    User user;
+}
