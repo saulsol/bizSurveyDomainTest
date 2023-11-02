4343f83db6814f1fac116cea52c6d0b7dd47d1d1
tud0 <psy563012@gmail.com>
2023-11-02 16:23:21 +0900

    enum 주석 추가, answer entity 추가

--- src/main/java/com/example/bizsurveydomainpr/constant/SurveyType.java
+++ src/main/java/com/example/bizsurveydomainpr/constant/SurveyType.java
@@ -2,6 +2,7 @@ package com.example.bizsurveydomainpr.constant;
 
 public enum SurveyType {
 
+    //설문지 기본형 normal, 점수형 score
     NORMAL,
     SCORE
 }
--- src/main/java/com/example/bizsurveydomainpr/constant/WorkspaceType.java
+++ src/main/java/com/example/bizsurveydomainpr/constant/WorkspaceType.java
@@ -1,7 +1,7 @@
 package com.example.bizsurveydomainpr.constant;
 
 public enum WorkspaceType {
-
+    //일반 personal, 기업 company
     PERSONAL, COMPANY
 
 }
